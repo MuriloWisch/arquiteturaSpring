4e5c8836c01bff49ef35d589bd863ed427d9f12b
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2025-09-04 16:45:19 -0300

    Add components pt2

--- arquiteturaspring/src/main/java/io/github/murilowisch/arquiteturaspring/todos/MailSender.java
+++ arquiteturaspring/src/main/java/io/github/murilowisch/arquiteturaspring/todos/MailSender.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MailSender {
-    public void envian(String mensagem){
+    public void enviar(String mensagem){
         System.out.println("Enviado email: " + mensagem);
     }
 }
